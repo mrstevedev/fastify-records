9e321ce244cdd3583092da97db3b90c270028ca6
Steven <stevendotpulido@gmail.com>
2025-03-25 22:51:56 -0700

    🔨refactor: AWS config with client-s3

--- src/client/aws.ts
+++ src/client/aws.ts
@@ -1,7 +1,9 @@
-import { S3 } from "aws-sdk";
+import { S3Client } from "@aws-sdk/client-s3";
 
-export const s3 = new S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION
+export const s3 = new S3Client({
+    region: process.env.AWS_REGION as string,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY as string
+    }
 });

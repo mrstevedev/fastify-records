9f51316aacc1b484715fc8c5294c92245e41f8c9
Steven <stevendotpulido@gmail.com>
2025-03-25 22:31:46 -0700

    ➕chore: Added @fastify/multipart

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify";
 import { CustomFastify } from "./types";
+import multipart from "@fastify/multipart";
 
 // Use declaration merging to add the new type to FastifyInstance
 declare module "fastify" {
@@ -7,6 +8,7 @@ declare module "fastify" {
         jwt: CustomFastify;
         bcrypt: CustomFastify;
     }
+
     interface FastifyReply {
         setCookie: (name: string, value: string, options?: any) => void;
     }
@@ -18,6 +20,9 @@ export const fastify: FastifyInstance = require("fastify")({
 
 export const PORT = process.env.PORT ? Number(process.env.PORT) : 9000;
 
+fastify.register(multipart, {
+    attachFieldsToBody: true
+});
 fastify.register(require("@fastify/swagger"));
 fastify.register(require("@fastify/swagger-ui"), {
     prefix: "/docs"
@@ -30,5 +35,4 @@ fastify.register(require("fastify-bcrypt"), {
 fastify.register(require("@fastify/jwt"), {
     secret: process.env.JWT_SECRET
 });
-fastify.register(require("@fastify/multipart"));
 fastify.register(require("@fastify/cookie"));
